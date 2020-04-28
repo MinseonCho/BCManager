a9d4a71d1d6c2668adf45357b40d82b99d6a340a
adorableMin <seoon53@gmail.com>
2020-04-29 01:42:52 +0900

    C++ 함수 호출 테스트
    C++ 함수 호출 테스트

--- app/src/main/java/com/example/bcmanager/MainActivity.java
+++ app/src/main/java/com/example/bcmanager/MainActivity.java
@@ -1,37 +1,47 @@
 package com.example.bcmanager;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.TargetApi;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.os.Build;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.SurfaceView;
-import android.view.WindowManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Mat;
+import org.opencv.features2d.BOWImgDescriptorExtractor;
+import org.opencv.imgproc.Imgproc;
 
-import java.util.Collections;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
 
-import static android.Manifest.permission.CAMERA;
+import static org.opencv.imgcodecs.Imgcodecs.imread;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static final String TAG = "opencv";
     private Mat matInput;
     private Mat matResult;
-
     private CameraBridgeViewBase mOpenCvCameraView;
+    private boolean isOpenCvLoaded = false;
+
+    //test
+    private ImageView inputImage;
+    private ImageView outputImage;
+    private Button btn_click;
 
     public native void ConvertRGBtoGray(long matAddrInput, long matAddrResult);
+
+    public native void BlurImage(long inputImage, long outputImage);
 
 
     static {
@@ -39,21 +49,21 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         System.loadLibrary("native-lib");
     }
 
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS: {
-                    mOpenCvCameraView.enableView();
-                }
-                break;
-                default: {
-                    super.onManagerConnected(status);
-                }
-                break;
-            }
-        }
-    };
+//    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+//        @Override
+//        public void onManagerConnected(int status) {
+//            switch (status) {
+//                case LoaderCallbackInterface.SUCCESS: {
+//                    mOpenCvCameraView.enableView();
+//                }
+//                break;
+//                default: {
+//                    super.onManagerConnected(status);
+//                }
+//                break;
+//            }
+//        }
+//    };
 
 
     @Override
@@ -63,49 +73,79 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
         //Hello Miyoung
 
+        inputImage = findViewById(R.id.inputimage);
+        outputImage = findViewById(R.id.outputimage);
+        btn_click = findViewById(R.id.click);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
-                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        inputImage.setImageResource(R.drawable.test_11);
 
-        setContentView(R.layout.activity_main);
+        btn_click.setOnClickListener(new View.OnClickListener() {
 
-        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.activity_surface_view);
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-        mOpenCvCameraView.setCvCameraViewListener(this);
-        mOpenCvCameraView.setCameraIndex(0); // front-camera(1),  back-camera(0)
+            @Override
+            public void onClick(View v) {
+                Log.d("테스트","테스트");
+                //메소드를 불러서 결과를 outputImage에 저장.
+                try {
+                    Log.d("테스트","테스트2");
+                    InputStream is = getAssets().open("test_11.jpg");
+                    Bitmap bitmap = BitmapFactory.decodeStream(is);
+
+                    Mat image = new Mat();
+                    Utils.bitmapToMat(bitmap, image);
+                    Mat output = new Mat();
+
+                    BlurImage(image.getNativeObjAddr(), output.getNativeObjAddr());
+
+                    Bitmap bitmapOutput = Bitmap.createBitmap(image.cols(), image.rows(), Bitmap.Config.ARGB_8888);
+                    Utils.matToBitmap(output, bitmapOutput);
+                    outputImage.setImageBitmap(bitmapOutput);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+//                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+//
+//        setContentView(R.layout.activity_main);
+//
+//        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.activity_surface_view);
+//        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+//        mOpenCvCameraView.setCvCameraViewListener(this);
+//        mOpenCvCameraView.setCameraIndex(0); // front-camera(1),  back-camera(0)
 
     }
 
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
 
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
 
-        if (!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "onResume :: Internal OpenCV library not found.");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
-        } else {
-            Log.d(TAG, "onResum :: OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
+//        if (!OpenCVLoader.initDebug()) {
+//            Log.d(TAG, "onResume :: Internal OpenCV library not found.");
+//            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
+//        } else {
+//            Log.d(TAG, "onResum :: OpenCV library found inside package. Using it!");
+//            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+//        }
     }
 
 
     public void onDestroy() {
         super.onDestroy();
 
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
+//        if (mOpenCvCameraView != null)
+//            mOpenCvCameraView.disableView();
     }
 
 
@@ -123,7 +163,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         matInput = inputFrame.rgba();
 
-        if ( matResult == null )
+        if (matResult == null)
 
             matResult = new Mat(matInput.rows(), matInput.cols(), matInput.type());
 
@@ -132,72 +172,72 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         return matResult;
     }
 
-    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
-        return Collections.singletonList(mOpenCvCameraView);
-    }
+//    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+//        return Collections.singletonList(mOpenCvCameraView);
+//    }
 
 
     //여기서부턴 퍼미션 관련 메소드
-    private static final int CAMERA_PERMISSION_REQUEST_CODE = 200;
-
-
-    protected void onCameraPermissionGranted() {
-        List<? extends CameraBridgeViewBase> cameraViews = getCameraViewList();
-        if (cameraViews == null) {
-            return;
-        }
-        for (CameraBridgeViewBase cameraBridgeViewBase: cameraViews) {
-            if (cameraBridgeViewBase != null) {
-                cameraBridgeViewBase.setCameraPermissionGranted();
-            }
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        boolean havePermission = true;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
-                havePermission = false;
-            }
-        }
-        if (havePermission) {
-            onCameraPermissionGranted();
-        }
-    }
-
-    @Override
-    @TargetApi(Build.VERSION_CODES.M)
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE && grantResults.length > 0
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            onCameraPermissionGranted();
-        }else{
-            showDialogForPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다.");
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-
-    @TargetApi(Build.VERSION_CODES.M)
-    private void showDialogForPermission(String msg) {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this);
-        builder.setTitle("알림");
-        builder.setMessage(msg);
-        builder.setCancelable(false);
-        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id){
-                requestPermissions(new String[]{CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
-            }
-        });
-        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface arg0, int arg1) {
-                finish();
-            }
-        });
-        builder.create().show();
-    }
+//    private static final int CAMERA_PERMISSION_REQUEST_CODE = 200;
+//
+//
+//    protected void onCameraPermissionGranted() {
+//        List<? extends CameraBridgeViewBase> cameraViews = getCameraViewList();
+//        if (cameraViews == null) {
+//            return;
+//        }
+//        for (CameraBridgeViewBase cameraBridgeViewBase: cameraViews) {
+//            if (cameraBridgeViewBase != null) {
+//                cameraBridgeViewBase.setCameraPermissionGranted();
+//            }
+//        }
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        boolean havePermission = true;
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            if (checkSelfPermission(CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//                requestPermissions(new String[]{CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+//                havePermission = false;
+//            }
+//        }
+//        if (havePermission) {
+//            onCameraPermissionGranted();
+//        }
+//    }
+//
+//    @Override
+//    @TargetApi(Build.VERSION_CODES.M)
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE && grantResults.length > 0
+//                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//            onCameraPermissionGranted();
+//        }else{
+//            showDialogForPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다.");
+//        }
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//    }
+//
+//
+//    @TargetApi(Build.VERSION_CODES.M)
+//    private void showDialogForPermission(String msg) {
+//
+//        AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this);
+//        builder.setTitle("알림");
+//        builder.setMessage(msg);
+//        builder.setCancelable(false);
+//        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface dialog, int id){
+//                requestPermissions(new String[]{CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+//            }
+//        });
+//        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface arg0, int arg1) {
+//                finish();
+//            }
+//        });
+//        builder.create().show();
+//    }
 }
--- app/src/main/jni/main.cpp
+++ app/src/main/jni/main.cpp
@@ -4,21 +4,98 @@
 #include <opencv2/opencv.hpp>
 
 
-using namespace cv;
+void filter(cv::Mat image, cv::Mat mat, cv::Mat mat1);
 
+using namespace cv;
 
 
 extern "C" {
 
-    JNIEXPORT void JNICALL Java_com_example_bcmanager_MainActivity_ConvertRGBtoGray(
-            JNIEnv *env, jobject instance, jlong matAddrInput, jlong matAddrResult) {
+JNIEXPORT void JNICALL Java_com_example_bcmanager_MainActivity_ConvertRGBtoGray(
+        JNIEnv *env, jobject instance, jlong matAddrInput, jlong matAddrResult) {
 
 
-        Mat &matInput = *(Mat *) matAddrInput;
-        Mat &matResult = *(Mat *) matAddrResult;
+    Mat &matInput = *(Mat *) matAddrInput;
+    Mat &matResult = *(Mat *) matAddrResult;
 
-        cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
+    cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
 
-
-    }
 }
+
+}
+extern "C"
+JNIEXPORT void JNICALL Java_com_example_bcmanager_MainActivity_BlurImage(JNIEnv *env, jobject thiz,
+                                                                         jlong input_image,
+                                                                         jlong output_image) {
+    // TODO: implement BlurImage()
+
+
+    float data_1[] = {
+            1 / 9.f, 1 / 9.f, 1 / 9.f,
+            1 / 9.f, 1 / 9.f, 1 / 9.f,
+            1 / 9.f, 1 / 9.f, 1 / 9.f
+    };
+
+    float data2[] = {
+            0,-1,0,
+            -1,5,-1,
+            0,-1,0
+    };
+
+    Mat &input = *(Mat*) input_image;
+    CV_Assert(input.data);
+    Mat &output = *(Mat*) output_image;
+
+//    cvtColor( input, output, COLOR_RGB2GRAY);
+
+//    Sobel(input, output, CV_32F, 1,0,3);
+//    convertScaleAbs(output, output);
+
+    Mat mask(3, 3, CV_32F, data_1);
+    Mat mask_2(3, 3, CV_32F, data2);
+
+    Mat blur_1;
+//    filter(input, output, mask);
+
+    output = Mat(input.size(), CV_32F, Scalar(0));
+    Point h_m = mask_2.size() / 2;
+
+    for (int i = h_m.y; i < input.rows - h_m.y; i++) {
+        for (int j = h_m.x; j < input.cols - h_m.y; j++) {
+
+            float sum = 0;
+            for (int u = 0; u < mask_2.rows; u++) {
+                for (int v = 0; v < mask_2.cols; v++) {
+                    int y = i + u - h_m.y;
+                    int x = j + v - h_m.x;
+                    sum += mask_2.at<float>(u, v) * input.at<uchar>(y, x); //회선수식
+                }
+            }
+
+            output.at<float>(i, j) = sum;
+        }
+    }
+
+    output.convertTo(output, CV_8U);
+}
+
+//void filter(Mat img, Mat& dst, Mat mask) {
+//    dst = Mat(img.size(), CV_32F, Scalar(0));
+//    Point h_m = mask.size() / 2;
+//
+//    for (int i = h_m.y; i < img.rows - h_m.y; i++) {
+//        for (int j = h_m.x; j < img.cols - h_m.y; j++) {
+//
+//            float sum = 0;
+//            for (int u = 0; u < mask.rows; u++) {
+//                for (int v = 0; v < mask.cols; v++) {
+//                    int y = i + u - h_m.y;
+//                    int x = j + v - h_m.x;
+//                    sum += mask.at<float>(u, v) * img.at<uchar>(y, x); //회선수식
+//                }
+//            }
+//
+//            dst.at<float>(i, j) = sum;
+//        }
+//    }
+//}
